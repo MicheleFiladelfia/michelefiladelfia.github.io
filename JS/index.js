a7fa6e3a5301ae80d471756592df063b971fafe9
Michele Filadelfia <86882607+MicheleFiladelfia@users.noreply.github.com>
2022-02-11 12:32:13 +0100

    Completely removed clarity

--- JS/index.js
+++ JS/index.js
@@ -2,15 +2,6 @@ let xType = 'img/x.png';
 let menuType = 'img/menu.png';
 let isMenuOpened = false;
 
-(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "aebwmnwm56");
-
-
-
-
 if(window.getComputedStyle( document.body ,null).getPropertyValue('background-color')=="rgb(34, 35, 34)"){
   document.getElementById("icon").src = "img/icon_dark.png";
   document.getElementById("menu").src ="img/menu_dark.png";
